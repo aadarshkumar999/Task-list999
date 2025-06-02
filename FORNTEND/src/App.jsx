d3864090ec957f9c164c79197878b46936e58fb6
delta <aadarshkishorsingh@gmail.com>
2025-06-02 21:47:24 +0530

    Fix case in PageNotFound filename

--- FORNTEND/src/App.jsx
+++ FORNTEND/src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Home from './components/Home'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import PageNotFound from './components/pageNotFound'
-import toast, { Toaster } from 'react-hot-toast';
+import React from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Navigate, Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function App() {
-  const token = localStorage.getItem("jwt")
+  const token = localStorage.getItem("jwt");
   return (
     <div>
-       <Routes>
-        <Route path='/' element={token ? <Home/> : <Navigate to={"/login"}/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='*' element={<PageNotFound/>}></Route>
-       
-       </Routes>
-       <Toaster />
+      <Routes>
+        <Route
+          path="/"
+          element={token ? <Home /> : <Navigate to={"/login"} />}
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+      <Toaster />
     </div>
-  )
+  );
 }
