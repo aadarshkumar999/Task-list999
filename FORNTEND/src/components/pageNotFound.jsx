313131ac0093fada973ec9cef31c0d25d50d37ad
delta <aadarshkishorsingh@gmail.com>
2025-06-02 16:30:50 +0530

    Updated PageNotFound.css

--- FORNTEND/src/components/pageNotFound.jsx
+++ FORNTEND/src/components/pageNotFound.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import './PageNotFound.css'; // Importing CSS styles
+import React from "react";
+import "./PageNotFound.css";
 
 // PageNotFound.js
 export default function PageNotFound() {
-    return (
-        <div className="container">
-        <div className="number">404</div>
-        <div className="text">
-            <span>Ooops...</span>
-            <br />page not found
-        </div>
-        <a 
-            className="me" 
-            href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" 
-            target="_blank" 
-            rel="noopener noreferrer"
-        >
-        </a>
-        
+  return (
+    <div className="container">
+      <div className="number">404</div>
+      <div className="text">
+        <span>Ooops...</span>
+        <br />
+        page not found
+      </div>
+      <a
+        className="me"
+        href="https://codepen.io/uzcho_/pens/popular/?grid_type=list"
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
     </div>
-);
-};
+  );
+}
 
 // PageNotFound;
